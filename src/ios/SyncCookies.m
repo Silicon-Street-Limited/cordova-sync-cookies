434bfb652820bf808ad395d4fdbfb566a8ee207b
SiliconStreetDev1 <rikardt@siliconst.co.nz>
2024-09-27 17:11:06 +1200

    ;;

--- src/ios/SyncCookies.m
+++ src/ios/SyncCookies.m
@@ -13,18 +13,7 @@
 
 - (void)SyncCookiesFromWK:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
-    @try {
-        WKWebsiteDataStore* dataStore = [WKWebsiteDataStore defaultDataStore];
-        WKHTTPCookieStore* cookieStore = dataStore.httpCookieStore;
-        [cookieStore getAllCookies:^(NSArray* cookies) {
-            [self syncCookies:cookies toStorage:[NSHTTPCookieStorage sharedHTTPCookieStorage]];
-        }];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Set cookie executed"];
-    } @catch (NSException *e) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:e.reason];
-    } @finally {
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
+
 }
 
 - (void)syncCookiesFromNS:(CDVInvokedUrlCommand*)command {
